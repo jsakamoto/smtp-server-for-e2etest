92cd2cab98a2bc398fa1e44ce2a7277cfe8c90ad
jsakamoto <junichi.jp@gmail.com>
2017-11-07 18:07:11 +0900

    Wire up "WaiteForEndOfAllSessions()"

--- SmtpServerForE2ETest/Services/SmtpService.cs
+++ SmtpServerForE2ETest/Services/SmtpService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Toolbelt.Net.Smtp.Services;
 
 namespace Toolbelt.Net.Smtp
@@ -55,7 +54,7 @@ namespace Toolbelt.Net.Smtp
 
         private void WaiteForEndOfAllSessions()
         {
-            // TODO: throw new NotImplementedException();
+            this.SmtpServer.WaiteForEndOfAllSessions();
         }
 
         public IEnumerable<SmtpMessage> GetAllMessages()
